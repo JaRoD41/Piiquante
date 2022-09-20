b32e5d6726dcf28eb54bf52cedcf703834f9bf27
JaRoD41 <laurent.trinco@gmail.com>
2022-09-20 17:07:22 +0200

    modif auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
@@ -5,9 +7,15 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.JWT_KEY);
     const userId = decodedToken.userId;
-    req.auth = {
-      userId: userId
-    };
+
+    req.auth = { userId };
+
+    if (req.body.userId && req.body.userId !== userId) {
+      throw process.env.AUTH_ID;
+    } else {
+      next();
+    }
+    
   } catch(error) {
     res.status(401).json({ error });
   }
