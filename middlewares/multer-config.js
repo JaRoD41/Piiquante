30e5b1a67267092f1ae8efdb371eec2d9d008f31
JaRoD41 <laurent.trinco@gmail.com>
2022-09-20 10:43:54 +0200

    add comments

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -1,10 +1,14 @@
 const multer = require('multer');
 
+//création d'un dictionnaire MIME_TYPES de types de fichiers
+
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
+
+//On crée un objet de configuration pour multer
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
@@ -16,5 +20,7 @@ const storage = multer.diskStorage({
     callback(null, name + Date.now() + '.' + extension);
   }
 });
+
+//On exporte le middleware multer avec l'objet storage
 
 module.exports = multer({ storage }).single('image');
