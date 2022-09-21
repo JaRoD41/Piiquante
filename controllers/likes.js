c5cd471ae352af7b1d0516bbec0b97c41214b23f
JaRoD41 <laurent.trinco@gmail.com>
2022-09-21 22:57:10 +0200

    like=1 OK

--- controllers/likes.js
+++ controllers/likes.js
@@ -3,17 +3,6 @@
 const Sauce = require('../models/Sauce');
 
 exports.likeDislikeSauce = (req, res) => {
-  console.log("je SUIS dans le controleur LIKE via POSTMAN !");
-
-  //je récupère l'id de la sauce affichée, l'id de l'utilisateur en cours et l'état du like en cours
-  
-  let likeChoice = req.body.like;
-  let userId = req.body.userId;
-  let pickedSauce = req.params.id;
-
-  //déclaration des futures conditions du ternaire pour la présence de l'user dans l'array
-  let isLiked = usersLiked.includes(userId);
-  let isDisliked = usersDisliked.includes(userId);
 
   /*la requète sera envoyée sous ce format dans body-->raw en JSON dans postman
   {
@@ -24,41 +13,58 @@ exports.likeDislikeSauce = (req, res) => {
 
   //mise en place d'un switch pour traiter les 3 cas possibles de like/dislike 
 
-  switch (likeChoice) {
-    case 1: 
-      console.log("picked sauce :", pickedSauce);
-      Sauce.updateOne(
-				{ _id: req.params.id },
-				{
-					$inc: { likes: likeChoice, dislikes: likeChoice },
-					$push: { usersLiked: userId },
-				}
-			)
-				.then((sauce) => {
-					console.log("contenu résultat promesse :", sauce);
-				})
-				.catch((error) => res.status(400).json({ error }));
+  Sauce.findOne({ _id: req.params.id })
+		.then((sauce) => {
+
+			//si le tableau des likes ne contient pas ce userId ET que le like vaut 1
+
+			if (!sauce.usersLiked.includes(req.body.userId) && req.body.like === 1) {
+				console.log("front like=1 et userId non présent dans usersLiked");
+
+				//Mise à jour de la base MongoDB
+
+				Sauce.updateOne(
+					{ _id: req.params.id },
+					{
+						$inc: { likes: 1 },
+						$push: { usersLiked: req.body.userId }
+					}
+				)
+					.then(() =>
+						res.status(201).json({ message: "like/dislike pris en compte !" })
+					)
+					.catch((error) => res.status(400).json({ error }));
+			}
+		})
+		.catch((error) => res.status(400).json({ error }));
+
+    
+
+  };
 
 
-      /*Sauce.updateOne(
-				{ _id: req.params.id },
-				{ likes: req.body.like, usersLiked: likedArray, _id: require.params.id }
-			)
-				.then(() => res.status(200).json({ message: "like pris en compte !" }))
-				.catch((error) => res.status(400).json({ error }));*/
-      break;
-    case -1:
-      console.log("je perds un like");
-      break;
-    case 0:
-      console.log("je suis neutre");
-  }
 
+  /*like = 0 (pas de vote, likes = 0 )
+//si le tableau des likes contient ce userId ET que le like vaut 0
 
+			if (sauce.usersLiked.includes(req.body.userId) && req.body.like === 0) {
+				console.log("front like=0 et userId présent dans usersLiked");
 
-  //like = 0 (pas de vote, likes = 0 )
+				//Mise à jour de la base MongoDB
 
-
+				Sauce.updateOne(
+					{ _id: req.params.id },
+					{
+						$inc: { likes: -1 }, //si la valeur du like vaut 0, on incrémente de -1
+						$pull: { usersLiked: req.body.userId },
+					}
+				)
+					.then(() =>
+						res.status(201).json({ message: "like/dislike vaut 0 !" })
+					)
+					.catch((error) => res.status(400).json({ error }));
+			}
+*/
 
 
   //like = -1 (dislikes = +1 )
@@ -70,4 +76,3 @@ exports.likeDislikeSauce = (req, res) => {
 
 
 
-};
